1d4459aa367699c5d450e32a87b52c58dbe287ec
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-01 18:01:17 +0100

    first draft complete

---
+++ dist/js/main.polyfills.js
@@ -0,0 +1,55 @@
+/*! project-name v0.0.1 | (c) 2020 Francuski Miroslav | MIT License | http://link-to-your-git-repo.com */
+$(document).ready((function() {
+  console.log("main.js file loaded");
+}));
+
+$(window).on("scroll", (function() {
+  if ($(window).scrollTop()) {
+    $(".nav-top").addClass("nav-top--sticky");
+  } else {
+    $(".nav-top").removeClass("nav-top--sticky");
+  }
+}));
+
+$(".menu-btn").on("click", (function() {
+  $("nav ul").toggleClass("showing");
+}));
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+
+$(document).on("click", ".nav ul li a", (function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+}));
+
+$(".portfolio-item").isotope({
+  itemSelector: ".item",
+  layoutMode: "fitRows"
+});
+
+$(".portfolio-menu ul li").click((function() {
+  $(".portfolio-menu ul li").removeClass("active");
+  $(this).addClass("active");
+
+  var selector = $(this).attr("data-filter");
+  $(".portfolio-item").isotope({
+    filter: selector
+  });
+  return false;
+}));
+
+function goBack() {
+  window.history.back();
+}
