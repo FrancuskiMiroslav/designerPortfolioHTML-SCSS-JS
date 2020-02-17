e9d59944f2a820dda118f6aef89ac543aa33b82c
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-18 00:53:32 +0100

    updated about page

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -49,6 +49,36 @@ $(".portfolio-menu ul li").click(function() {
   return false;
 });
 
+// skill bar ABOUT PAGE
+$(".skillbar").skillBars({
+  // number start
+  from: 0,
+
+  // number end
+  to: false,
+
+  // animation speed
+  speed: 3000,
+
+  // how often the element should be up<a href="https://www.jqueryscript.net/time-clock/">date</a>d
+  interval: 100,
+
+  // the number of decimal places to show
+  decimals: 0,
+
+  // callback method for every time the element is updated,
+  onUpdate: null,
+
+  // callback method for when the element finishes updating
+  onComplete: null,
+
+  // CSS classes
+  classes: {
+    skillBarBar: ".skillbar-bar",
+    skillBarPercent: ".skill-bar-percent"
+  }
+});
+
 function goBack() {
   window.history.back();
 }
